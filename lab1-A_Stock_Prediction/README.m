1d06e75c491447f00ce7b4abcc8bcc9fef1c4ca7
Khan Chou <100106915+Khansakura@users.noreply.github.com>
2023-06-10 10:03:06 +0800

    Create README.m

---
+++ lab1-A_Stock_Prediction/README.m
@@ -0,0 +1,4 @@
+A Stock Prediction
+Enter the closing price of a stock for the first 14 trading days and predict the closing price for the next trading day.  
+The experimental indicators are average absolute percentage error (MAPE) and average absolute error (MAE).  
+Base on python `Pandas` 、 `Numpy` 、`Scikit-learn` and `Keras`、`TensorFlow`、`PyTorch`  
