4511bbd5a841e48588c8bb08ea4e5ceb3ccbc489
Khan Chou <100106915+Khansakura@users.noreply.github.com>
2023-06-10 10:04:57 +0800

    Update README.m

--- lab1-A_Stock_Prediction/README.m
+++ lab1-A_Stock_Prediction/README.m
@@ -2,3 +2,5 @@ A Stock Prediction
 Enter the closing price of a stock for the first 14 trading days and predict the closing price for the next trading day.  
 The experimental indicators are average absolute percentage error (MAPE) and average absolute error (MAE).  
 Base on python `Pandas` 、 `Numpy` 、`Scikit-learn` and `Keras`、`TensorFlow`、`PyTorch`  
+   
+Final: MAPE=0.0215, MAE=0.5978. Not bad I think, but they can still be improved by choosing better parameter, eta.
